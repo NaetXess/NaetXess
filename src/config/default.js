37141bf280b056d2916ce08ce2576ee14dcf460c
NaetXess <91805652+NaetXess@users.noreply.github.com>
2023-12-27 18:42:52 +0300

    Delete src directory

--- src/config/default.js
+++
@@ -1,9 +0,0 @@
-let apiUrl;
-
-if (import.meta.env.VITE_LOCATION === "RELEASED") {
-  apiUrl = "";
-} else {
-  apiUrl = "http://localhost:1010";
-}
-
-export { apiUrl };
--- src/pages/_layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { _ } from "svelte-i18n";
-
-  export async function preload() {
-    return waitLocale();
-  }
-</script>
-
-<slot />
